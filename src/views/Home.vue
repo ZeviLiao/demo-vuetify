58ef20eae30c56a128daaadfe0e56b069f443f25
Zevi Liao <jointjm@gmail.com>
2019-06-29 07:36:28 +0800

    btn

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,29 @@
 <template>
-  <HelloWorld />
+  <div class="home">
+    <h1>This is an home page</h1>
+    <v-btn class="pink white--text">click me</v-btn>
+    <v-btn depressed color="pink">click me</v-btn>
+    <v-btn flat color="pink">click me</v-btn>
+    <v-btn depressed class="pink white--text">
+      <v-icon left>email</v-icon>
+      <span>eamil me</span>
+    </v-btn>
+    <v-btn depressed small class="pink white--text">
+      <v-icon left small>email</v-icon>
+      <span>eamil me</span>
+    </v-btn>
+    <v-btn depressed small class="pink white--text">
+      <v-icon left small>email</v-icon>
+      <span>eamil me</span>
+    </v-btn>
+    <v-btn fab depressed small dark class="purple">
+      <v-icon>favorite</v-icon>
+    </v-btn>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+export default {
 
-  export default {
-    components: {
-      HelloWorld
-    }
-  }
+}
 </script>
