c863ec98f5083c3f0e90523811648fb6a113c90c
Zevi Liao <jointjm@gmail.com>
2019-06-29 09:26:19 +0800

    cards

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -2,8 +2,37 @@
   <div class="about">
     <h1 class="subheading grey--text">Team</h1>
     <v-container fluid class="my-5">
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem quia voluptatum doloremque distinctio. Assumenda explicabo maiores necessitatibus, asperiores, modi ad ullam quaerat repellat, similique eius voluptatem sed vero nesciunt minima.</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem quia voluptatum doloremque distinctio. Assumenda explicabo maiores necessitatibus, asperiores, modi ad ullam quaerat repellat, similique eius voluptatem sed vero nesciunt minima.</p>
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
+          <v-card flat class="text-xs-center ma-3">
+            <v-responsive class="pt-4">image goes here</v-responsive>
+            <v-card-text>
+              <div class="subheading">{{person.name}}</div>
+              <div class="grey--text">{{person.name}}</div>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
     </v-container>
   </div>
 </template>
+
+<script>
+  export default {
+      data(){
+        return {
+          team:[
+            {name:'a', role:'aa'},
+            {name:'a', role:'aa'},
+            {name:'a', role:'aa'},
+            {name:'a', role:'aa'},
+            {name:'a', role:'aa'},
+          ]
+        }
+      }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
