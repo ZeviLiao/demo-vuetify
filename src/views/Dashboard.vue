63a779f62861338f23af7b9b9f3cc47d91420cfe
Zevi Liao <jointjm@gmail.com>
2019-06-29 08:48:42 +0800

    grid system

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,29 +1,15 @@
 <template>
-  <div class="home">
-    <h1>This is an home page</h1>
-    <v-btn class="pink white--text">click me</v-btn>
-    <v-btn depressed color="pink">click me</v-btn>
-    <v-btn flat color="pink">click me</v-btn>
-    <v-btn depressed class="pink white--text">
-      <v-icon left>email</v-icon>
-      <span>eamil me</span>
-    </v-btn>
-    <v-btn depressed small class="pink white--text">
-      <v-icon left small>email</v-icon>
-      <span>eamil me</span>
-    </v-btn>
-    <v-btn depressed small class="pink white--text">
-      <v-icon left small>email</v-icon>
-      <span>eamil me</span>
-    </v-btn>
-    <v-btn fab depressed small dark class="purple">
-      <v-icon>favorite</v-icon>
-    </v-btn>
+  <div class="dashboard">
+    <h1 class="subheading grey--text">Dashboard</h1>
+    <v-container class="my-5">
+      <v-layout row wrap>
+        <v-flex xs12 md6>
+          <v-btn outline block class="primary">1</v-btn>
+        </v-flex>
+        <v-flex xs12 md6>
+          <v-btn outline block class="primary">1</v-btn>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
-
-<script>
-export default {
-
-}
-</script>
