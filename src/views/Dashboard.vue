31c09d21f03530ed70382c51b43a19cce931a17b
Zevi Liao <jointjm@gmail.com>
2019-06-29 08:50:57 +0800

    update

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -6,8 +6,14 @@
         <v-flex xs12 md6>
           <v-btn outline block class="primary">1</v-btn>
         </v-flex>
-        <v-flex xs12 md6>
-          <v-btn outline block class="primary">1</v-btn>
+        <v-flex xs4 md2>
+          <v-btn outline block class="primary">2</v-btn>
+        </v-flex>
+        <v-flex xs4 md2>
+          <v-btn outline block class="primary">2</v-btn>
+        </v-flex>
+        <v-flex xs4 md2>
+          <v-btn outline block class="primary">2</v-btn>
         </v-flex>
       </v-layout>
     </v-container>
