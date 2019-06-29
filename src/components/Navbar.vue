4d5afe9987da3e1e3eb1abe6a22ff13b2d49529f
Zevi Liao <jointjm@gmail.com>
2019-06-29 08:20:44 +0800

    nav list

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -12,10 +12,19 @@
         <v-icon right>exit_to_app</v-icon>
       </v-btn>
     </v-toolbar>
-    
 
     <v-navigation-drawer v-model="drawer" app class="indigo">
-      <p>test</p>
+      <v-list>
+        <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-tile-action>
+            <v-icon class="white--text">{{link.icon}}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title class="white--text">{{link.text}}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
     </v-navigation-drawer>
   </nav>
 </template>
@@ -24,7 +33,12 @@
 export default {
   data() {
     return {
-      drawer: false
+      drawer: false,
+      links: [
+        { icon: 'dashboard', text: 'Dashboard', route: '/' },
+        { icon: 'folder', text: 'My Project', route: '/projects' },
+        { icon: 'person', text: 'Team', route: '/team' }
+      ]
     }
   }
 }
