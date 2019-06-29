d8bad56d8c6de7f5250223c48b687de9356f72fc
Zevi Liao <jointjm@gmail.com>
2019-06-29 08:41:26 +0800

    update

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
-    <h1>This is an Team page</h1>
+    <h1 class="subheading grey--text">Team</h1>
+    <v-container fluid class="my-5">
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem quia voluptatum doloremque distinctio. Assumenda explicabo maiores necessitatibus, asperiores, modi ad ullam quaerat repellat, similique eius voluptatem sed vero nesciunt minima.</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem quia voluptatum doloremque distinctio. Assumenda explicabo maiores necessitatibus, asperiores, modi ad ullam quaerat repellat, similique eius voluptatem sed vero nesciunt minima.</p>
+    </v-container>
   </div>
 </template>
